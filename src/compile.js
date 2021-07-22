f5dde3693dda9f0fcf915e7b5406dbee4cad25fc
Alejandro Sanchez <aalejo@gmail.com>
2021-07-22 16:12:31 -0400

    fixed socket.openWindow

--- src/compile.js
+++ src/compile.js
@@ -35,11 +35,8 @@ module.exports = {
     const foundErrors = [].concat(errors.filter(e => e !== null))
     if(foundErrors.length > 0) throw CompilationError(foundErrors.map(e => e.message).join(""))
     
-    let url = `${configuration.address}:${configuration.port}/preview`;
-    if(configuration.editor.mode === 'gitpod'){
-      url = `https://${configuration.port}-${configuration.address.substring(8)}/preview`
-    }
-    socket.emit('openWindow', status='ready', logs=[`Opening ${url}`], inputs=[], report=[], data=url)
+    socket.openWindow(configuration.publicUrl)
+
     return Utils.cleanStdout("Successfully built your HTML")
   },
 }
